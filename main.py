a8d65eca58660624f5d174d3218ad9ff6763d552
cdogmurph <cwmurphy@hotmail.co.uk>
2020-12-01 22:47:42 +0000

    advent code day 1

--- main.py
+++ main.py
@@ -4,17 +4,9 @@ star_list = content.split("\n")
 my_file.close()
 
 for x in star_list:
-  star1=int(x)
-  for y in star_list:
-    star2=int(y)
-    if star1+star2 == 2020:
-      print("thing one: {0}, thing two: {1}".format(star1, star2))
-      break
-
-
-
-
-
-
-
-
+    star1 = int(x)
+    for y in star_list:
+        star2 = int(y)
+        if star1 + star2 == 2020:
+            print("thing one: {0}, thing two: {1}".format(star1, star2))
+            break
